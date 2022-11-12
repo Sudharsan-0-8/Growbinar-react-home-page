14d6a24eecd2982631a874570f6c3eba404d3af3
Sudharsan-0-8 <116365632+Sudharsan-0-8@users.noreply.github.com>
2022-11-12 12:46:29 +0530

    LOGO - 1 file modified, 1 added, 1 deleted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import '../styles/header.css';
+// import Logo from '../assets/Grow_Logo.jpg';
 
 function Header() {
   return (
     <div className="home_header">
       <div className="header_left">
         <div className="logo">
+          <img src={require('./assets/logo.jpg')} />
           <strong>
             <span style={{ color: '#6c60fe' }}>Grow</span>binar.
           </strong>
