b960a90b88c71f13a402207a6db348ba97d99457
Sudharsan-0-8 <116365632+Sudharsan-0-8@users.noreply.github.com>
2022-11-12 12:28:03 +0530

    home body added - 5 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import './styles/app.css';
 
 import Home from './components/Home.js';
 import Header from './components/Header.js';
@@ -7,8 +8,8 @@ import Header from './components/Header.js';
 export default function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Header className="app_header" />
+      <Home className="app_body" />
     </div>
   );
 }
